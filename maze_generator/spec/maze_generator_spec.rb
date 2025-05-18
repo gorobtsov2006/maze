00c79f4de022cf3fefd86d4dcb0694f6d8fa33b9
SxnCHeZzz <128973490+SxnCHeZzz@users.noreply.github.com>
2025-05-18 14:05:37 +0300

    Add maze generation with Recursive Backtracking and tests

--- maze_generator/spec/maze_generator_spec.rb
+++ maze_generator/spec/maze_generator_spec.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
-RSpec.describe MazeGenerator do
-  it "has a version number" do
-    expect(MazeGenerator::VERSION).not_to be nil
-  end
+  require "spec_helper"
+  require "set"
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  RSpec.describe MazeGenerator::Maze do
+    describe "#initialize" do
+      it "raises error for non-positive width" do
+        expect { described_class.new(width: 0, height: 5) }.to raise_error(ArgumentError, "Width must be positive")
+      end
+
+      it "raises error for non-positive height" do
+        expect { described_class.new(width: 5, height: 0) }.to raise_error(ArgumentError, "Height must be positive")
+      end
+
+      it "generates a valid maze grid" do
+        maze = described_class.new(width: 3, height: 3)
+        grid = maze.instance_variable_get(:@grid)
+        expect(grid).to be_an(Array)
+        expect(grid.size).to eq(7)
+        expect(grid[0].size).to eq(7)
+        expect(grid.flatten).to include(" ", "#")
+      end
+    end
   end
-end
